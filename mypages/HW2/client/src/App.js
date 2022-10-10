1cfff6f5a1d5133a1e5c6f093197b6b24cc6eca0
yuancc12 <106596798+yuancc12@users.noreply.github.com>
2022-10-10 21:16:26 +0800

    delete

---
+++ mypages/HW2/client/src/App.js
@@ -0,0 +1,54 @@
+
+import { useState } from 'react';
+import './App.css';
+import Axios from "axios";
+function App() {
+  const[name,setname]=useState("");
+  const[age,setage]=useState(0);
+  const[position,setposition]=useState("");
+  const[country,setcountry]=useState("");
+  const[wage,setwage]=useState(0);
+
+  const addEmployee=()=>{
+    Axios.post("http://localhost:3001/create",
+    {
+      name:name,
+      age:age,
+      country:country,
+      position:position,
+      wage:wage,
+    }).then(() => {
+      console.log("success")
+  });
+};
+  return (
+    <div className="App">
+      <div className="information">
+        <label>name</label>
+        <input type="text" onChange={(event) => {
+        setname(event.target.value)
+        }}/>
+        <label>age</label>
+        <input type="number" onChange={(event) => {
+        setage(event.target.value)
+        }}/>
+        <label>position</label>
+        <input type="text" onChange={(event) => {
+        setposition(event.target.value)
+        }}/>
+        <label>country</label>
+        <input type="text" onChange={(event) => {
+        setcountry(event.target.value)
+        }}/>
+        <label>wage</label>
+        <input type="number" onChange={(event) => {
+        setwage(event.target.value)
+        }}/>
+        <button onClick={addEmployee} >Add</button>
+        </div>
+     
+    </div>
+  );
+};
+
+export default App;
---
+++ mypages/HW2/server/index.js
@@ -0,0 +1,35 @@
+const express=require("express")
+const app=express()
+const mysql=require("mysql")
+const cors=require("cors")
+
+app.use(cors());
+app.use(express.json());
+const db=mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    password:"20020416syc",
+    database:"employeeSystem",
+});
+app.post("/create",(req,res) =>{
+    const name=req.body.name;
+    const age=req.body.age;
+    const country=req.body.country;
+    const position=req.body.position;
+    const wage=req.body.wage;
+
+    db.query("INSERT INTO employees(name,age country,position,wage) VALUES (?,?,?,?,?)",
+    [name,age,country,position,wage],
+    (err,result) =>{
+        if (err){
+            console.log(err)
+        }else{
+            res.send("values Inserted")
+        }
+    }
+    );
+
+});
+app.listen(3001,()=>{
+    console.log("yes,your server is port 3001")
+})
